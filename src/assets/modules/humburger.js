832c21dc2c2a085e26b391dc854a10d925f86a83
Bondar Pavel <bondpv@yandex.ru>
2022-10-04 16:30:21 +0300

    feat: hamburger menu

--- src/assets/modules/humburger.js
+++ src/assets/modules/humburger.js
@@ -1,3 +1,20 @@
 'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header'),
+    menuItem = document.querySelectorAll('.nav-menu__item'),
+    hamburger = document.querySelector('.hamburger');
+    
 
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header_active');
+        });
+    });
+});
