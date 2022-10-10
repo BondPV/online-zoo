1f9b25826f464159c60eede6690ef6178aa369ed
Bondar Pavel <bondpv@yandex.ru>
2022-10-10 09:45:29 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 'use strict';
 import './assets/modules/humburger';
 import './assets/modules/pets_carousel';
-import './assets/modules/testimonial_popup';
 import './assets/modules/testimonials_carousel';
+import './assets/modules/testimonial_popup';
